63ef7b5128d236c942d0edb3c3a09f7e02ebac39
xenzhe <333554682+xenzhe@users.noreply.github.com>
2026-09-25 21:31:28 -0300

    fix: send item rarity to the UI

--- client.lua
+++ client.lua
@@ -1236,7 +1236,8 @@ RegisterNetEvent('ox_inventory:setPlayerInventory', function(currentDrops, inven
 			description = v.description,
 			buttons = buttons,
 			ammoName = v.ammoname,
-			image = v.client?.image
+			image = v.client?.image,
+			rarity = v.rarity
 		}
 	end
 

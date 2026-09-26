5e20ff3bdf2f56c0bdf3d16fbb49234142895314
xenzhe <333554682+xenzhe@users.noreply.github.com>
2026-09-26 00:51:55 -0300

    chore: release 1.0.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,9 +3,9 @@ use_experimental_fxv2_oal 'yes'
 lua54 'yes'
 game 'gta5'
 name 'ox_inventory'
-author 'Overextended'
-version '2.47.9'
-repository 'https://github.com/overextended/ox_inventory'
+author 'Xenzhe, Overextended'
+version '1.0.0'
+repository 'https://github.com/xenzhe/ox_inventory'
 description 'Slot-based inventory with item metadata support'
 
 dependencies {
